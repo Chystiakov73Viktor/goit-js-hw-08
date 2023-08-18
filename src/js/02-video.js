9128e1f8600f43b7cf0119a3453ad45c28871a8e
Chystiakov73Viktor <130795120+Chystiakov73Viktor@users.noreply.github.com>
2023-08-18 20:36:19 +0300

    add markup

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframeEl = document.querySelector('iframe');
+const player = new Vimeo.Player(iframeEl);
+
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player.on('timeupdate', throttle(onVideo, 1000));
+
+const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
+
+player.setCurrentTime(currentTime).then(function (seconds) {
+}).catch(function (error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
